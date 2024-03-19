088e8f72f59c8ef26724846e2ab99dc5a321b60a
CarlinGebyte <Carlos.Garcia@builtglobal.com>
2024-03-19 08:29:33 -0500

    Create routes - Add layout - New login/register pages - Add base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "drr-logo": "url('/src/assets/drr-logo.png')",
+        "main-layout": "url('/src/assets/main-bg.jpg')"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
-
