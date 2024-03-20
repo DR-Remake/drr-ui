ba3e014d5c7220becd9ad1c31ac5d68c4e3275a1
CarlinGebyte <Carlos.Garcia@builtglobal.com>
2024-03-20 17:24:27 -0500

    Update register form

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#1e0808",
+        border: "#8d8b8c"
+      },
       backgroundImage: {
         "drr-logo": "url('/src/assets/drr-logo.png')",
-        "main-layout": "url('/src/assets/main-bg.jpg')"
+        "main-layout": "url('/src/assets/main-bg.jpg')",
+        footer: "url('/src/assets/footer-bg.png')",
+        modal: "url('/src/assets/modal-bg.png')"
       }
     }
   },
