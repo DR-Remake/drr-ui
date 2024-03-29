46b4256affb6825de0bf21f279c49775a477c5fb
CarlinGebyte <Carlos.Garcia@builtglobal.com>
2024-03-29 17:53:19 -0500

    Create new reusable Input field

---
+++ src/components/Input.tsx
@@ -0,0 +1,27 @@
+import { Controller, FieldValues, Path, useFormContext } from "react-hook-form";
+import { cn } from "../lib/utils";
+
+export function Input<T extends FieldValues>({ name, label }: { name: Path<T>; label: string }) {
+  const { control } = useFormContext<T>();
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field, fieldState: { error } }) => (
+        <div className="flex w-full flex-col gap-1.5">
+          <label htmlFor={name} className={cn({ "text-red-500": error })}>
+            {label}
+          </label>
+          <input
+            id={name}
+            className={cn("w-full rounded-md border-2 border-gray-300 px-4 py-2 text-black focus:outline-none", {
+              "border-red-500": error
+            })}
+            {...field}
+          />
+          <span className={cn("text-xs", { "text-red-500": error })}>{error && error.message}</span>
+        </div>
+      )}
+    />
+  );
+}
--- src/routes/register.lazy.tsx
+++ src/routes/register.lazy.tsx
@@ -1,8 +1,9 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { z } from "zod";
 import borderImage from "../assets/border.svg";
+import { Input } from "../components/Input";
 import { RegisterSchema } from "../types/register";
 
 export const Route = createLazyFileRoute("/register")({
@@ -10,60 +11,35 @@ export const Route = createLazyFileRoute("/register")({
 });
 
 function Register() {
-  const {
-    handleSubmit,
-    register,
-    formState: { errors }
-  } = useForm<z.infer<typeof RegisterSchema>>({
+  const form = useForm<z.infer<typeof RegisterSchema>>({
     resolver: zodResolver(RegisterSchema)
   });
 
-  const submitHandler = handleSubmit((data) => {
+  const submitHandler = form.handleSubmit((data) => {
     console.log(data);
   });
 
   return (
-    <form
-      onSubmit={submitHandler}
-      className="mx-auto flex w-2/4 flex-col items-start gap-4 rounded-md border-4 bg-primary p-8"
-      style={{
-        border: "20px solid",
-        borderImage: `url(${borderImage}) 20`,
-        borderImageSlice: "30"
-      }}
-    >
-      <div className="text-xl font-bold">Register</div>
-      <input
-        {...register("username")}
-        placeholder="Username"
-        type="text"
-        className="w-full rounded-md border border-gray-300 px-4 py-2 text-black focus:outline-none"
-      />
-      <span>{errors.username?.message}</span>
-      <input
-        {...register("email")}
-        placeholder="Email"
-        type="email"
-        className="w-full rounded-md border border-gray-300 px-4 py-2 text-black focus:outline-none"
-      />
-      <span>{errors.email?.message}</span>
-      <input
-        {...register("password")}
-        placeholder="Password"
-        type="password"
-        className="w-full rounded-md border border-gray-300 px-4 py-2 text-black focus:outline-none"
-      />
-      <span>{errors.password?.message}</span>
-      <input
-        {...register("passwordConfirmation")}
-        placeholder="Confirm Password"
-        type="password"
-        className="w-full rounded-md border border-gray-300 px-4 py-2 text-black focus:outline-none"
-      />
-      <span>{errors.passwordConfirmation?.message}</span>
-      <button className="rounded-md border border-gray-50 px-4 py-2" type="submit">
-        Register
-      </button>
-    </form>
+    <FormProvider {...form}>
+      <div
+        className="mx-auto flex w-2/4 flex-col items-start gap-4 rounded-md border-4 bg-primary p-8"
+        style={{
+          border: "20px solid",
+          borderImage: `url(${borderImage}) 20`,
+          borderImageSlice: "30"
+        }}
+      >
+        <div className="mx-auto text-2xl font-bold">Create New Account</div>
+        <form onSubmit={submitHandler} className="w-full space-y-4">
+          <Input label="Username" name="username" />
+          <Input label="Email" name="email" />
+          <Input label="Password" name="password" />
+          <Input label="Confirm Password" name="passwordConfirmation" />
+          <button className="rounded-md border border-gray-50 px-4 py-2" type="submit">
+            Register
+          </button>
+        </form>
+      </div>
+    </FormProvider>
   );
 }
