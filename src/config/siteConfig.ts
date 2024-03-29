83edce20beb463693f29a846b02d38b2b6f4b108
CarlinGebyte <Carlos.Garcia@builtglobal.com>
2024-03-29 10:50:55 -0500

    Update root layout

---
+++ src/config/siteConfig.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  title: "My Site",
+  description: "This is my site",
+  navbar: [
+    {
+      label: "Home",
+      path: "/"
+    },
+    {
+      label: "Login",
+      path: "/login"
+    },
+    {
+      label: "Register",
+      path: "/register"
+    }
+  ]
+};
